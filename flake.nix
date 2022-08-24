b7942d1eac5553f5772a20b6b32b31c841bb8b4f
Yonathan Randolph <yonathan@gmail.com>
2022-08-24 08:31:56 -0700

    Add nix flake
    This may not match the current release since there is no lock file. So I think this is an upgraded aws-sdk-go

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      defaultPackage = pkgs.callPackage ./default.nix {};
+    }
+  );
+}
